a9de5c2807e771f2c5b8be2f1fe4ee9eb0f27cb3
Daniel Watford <dan@foomoo.com>
2014-05-17 11:12:03 +0100

    Implemented model tick

--- src/main/scala/com/foomoo/gol/model/GofModel.scala
+++ src/main/scala/com/foomoo/gol/model/GofModel.scala
@@ -3,36 +3,43 @@ package com.foomoo.gol.model
 import scala.language.postfixOps
 
 /**
- * Represents the alive cells in a generation of Game Of Life.
+ * Represents the live cells in a generation of Game Of Life.
  */
-class GofModel (cells: Set[Cell]) {
+class GofModel(cells: Set[Cell], cellFilter: Cell => Boolean) {
+
+  /**
+   * Returns the set of cells currently live in this model and valid according to the cellFilter.
+   */
+  lazy val getCells: Set[Cell] = cells.filter(cellFilter)
 
   /**
    * Given a cell, return the neighbouring live cells.
    */
   def liveNeighbours(cell: Cell): Seq[Cell] = {
-    cell.neighbouringCells filter(cells.contains(_))
+    cell.neighbouringCells filter (cells.contains(_))
   }
-  
+
   /**
    * The set of dead cells which have a live cell as a neighbour.
    */
-  lazy val deadCellsWithLiveNeighbours: Set[Cell] = cells flatMap(_.neighbouringCells) toSet 
-  
+  lazy val deadCellsWithLiveNeighbours: Set[Cell] = (getCells flatMap (_.neighbouringCells) toSet) filter cellFilter
+
   /**
-   * Perform a tick, returning a new GofModel representing all cells that are live after the tick.
+   * A new GofModel representing all cells that are live after the tick.
    */
-  def tick(): Unit = {
-    // Build a set of all dead cells which are neighbours to live cells.
-    // These will be candidate cells to become live if they have exactly 3 live neighbours.
-    //val candidateCellsForLife = deadCellsWithLiveNeighbours
+  lazy val tick: GofModel = {
+    val keepAliveCells = getCells.filter { cell =>
+      val liveNeighbourCount = liveNeighbours(cell).size
+      liveNeighbourCount == 2 || liveNeighbourCount == 3
+    }
     
-    //val mutMap: scala.collection.mutable.HashMap = 
+    val bringToLifeCells = deadCellsWithLiveNeighbours.filter(liveNeighbours(_).size == 3)
+
+    new GofModel(keepAliveCells ++ bringToLifeCells, cellFilter)
   }
 }
 
 object GofModel {
-  def apply(cells: Cell*): GofModel = {
-    new GofModel(cells.toSet)
-  }
+  def apply(cells: Cell*): GofModel = new GofModel(cells.toSet, (_ => true))
+  def apply(cellFilter: Cell => Boolean, cells: Cell*): GofModel = new GofModel(cells.toSet, cellFilter)
 }
--- src/test/scala/com/foomoo/gol/model/GofModelTest.scala
+++ src/test/scala/com/foomoo/gol/model/GofModelTest.scala
@@ -4,37 +4,73 @@ import com.foomoo.gol.UnitSpec
 
 class GofModelTest extends UnitSpec {
 
-  def fixture = new {
-
-    val model = {
-
-      GofModel(Cell(0, 0), Cell(2, 2))
-    }
-  }
-
   "A GofModel" should "find all live neighbouring cells to a given cell" in {
-    val f = fixture
-
-    var liveNeighbours = f.model.liveNeighbours(Cell(0, 1))
+    var liveNeighbours = GofModel(Cell(0, 0), Cell(2, 2)).liveNeighbours(Cell(0, 1))
     liveNeighbours should have size 1
-    liveNeighbours.contains(Cell(0, 0)) should be(true)
+    liveNeighbours should contain(Cell(0, 0))
 
-    liveNeighbours = f.model.liveNeighbours(Cell(1, 1))
+    liveNeighbours = GofModel(Cell(0, 0), Cell(2, 2)).liveNeighbours(Cell(1, 1))
     liveNeighbours should have size 2
-    liveNeighbours.contains(Cell(0, 0)) should be(true)
-    liveNeighbours.contains(Cell(2, 2)) should be(true)
+    liveNeighbours should (contain(Cell(0, 0)) and contain(Cell(2, 2)))
   }
 
   it should "find all dead cells neighbouring live cells" in {
     val expectedDead = (Cell(0, 0).neighbouringCells ++ Cell(2, 2).neighbouringCells).toSet
-    val f = fixture
 
-    val deadCells = f.model.deadCellsWithLiveNeighbours
+    val deadCells = GofModel(Cell(0, 0), Cell(2, 2)).deadCellsWithLiveNeighbours
     deadCells should have size 15
     deadCells.toSet should equal(expectedDead)
   }
-  
+
   it should "not return any live cells when searching for dead cells with live neighbours" in {
-    
+    GofModel(Cell(0, 0), Cell(2, 2)).deadCellsWithLiveNeighbours should not(contain(Cell(0, 0)) or contain(Cell(2, 2)))
+  }
+
+  it should "filter cells" in {
+    GofModel((_ => false), Cell(0, 0), Cell(1, 1), Cell(2, 2)).getCells should be('empty)
+
+    GofModel((_.y > 1), Cell(0, 0), Cell(1, 1), Cell(2, 2)).getCells should not(contain(Cell(0, 0)) or contain(Cell(1, 1)))
+  }
+
+  it should "cause a single cell model to an empty model" in {
+    GofModel(Cell(0, 0)).tick.getCells should be('empty)
+  }
+
+  it should "cause a cell with fewer than two neighbours to die" in {
+    GofModel(Cell(0, 0), Cell(0, 1), Cell(0, 2)).tick.getCells should not(contain(Cell(0, 0)) or contain(Cell(0, 2)))
+  }
+
+  it should "keep a cell with two neighbours live" in {
+    GofModel(Cell(0, 0), Cell(0, 1), Cell(0, 2)).tick.getCells should contain(Cell(0, 1))
+  }
+
+  it should "keep a cell with three neighbours live" in {
+    GofModel(Cell(0, 0), Cell(0, 2), Cell(1, 1), Cell(2, 2)).tick.getCells should contain(Cell(1, 1))
+  }
+
+  it should "kill a cell with four neighbours" in {
+    GofModel(Cell(1, 1), Cell(1, 0), Cell(1, 2), Cell(0, 1), Cell(2, 1)).tick.getCells should not contain (Cell(1, 1))
+  }
+
+  it should "kill a cell with eight neighbours" in {
+    val cell = Cell(1, 1)
+    val neighbours = cell.neighbouringCells;
+    GofModel(cell +: neighbours: _*).tick.getCells should not contain (Cell(1, 1))
+  }
+
+  it should "bring a dead cell with three live neighbours to life" in {
+    GofModel(Cell(0, 0), Cell(0, 2), Cell(2, 2)).tick.getCells should contain(Cell(1, 1))
+  }
+
+  "An Empty GofModel" should "return no cells" in {
+    GofModel().getCells should be('empty)
+  }
+
+  it should "return no live neighbours" in {
+    GofModel().liveNeighbours(Cell(0, 0)) should be('empty)
+  }
+
+  it should "return no dead cells with live neighbours" in {
+    GofModel().deadCellsWithLiveNeighbours should be('empty)
   }
 }
